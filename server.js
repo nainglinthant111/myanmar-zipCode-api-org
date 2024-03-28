7cbb3e05b862f5349e70802737b93a3abb8bd38c
nainglinthant <oppositedirection111@gmail.com>
2024-03-28 08:57:48 +0630

    update server

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// Import required modules
 const express = require('express');
 const connectDB = require('./Db/mongo');
 const { positalCode } = require('./Model/code');
@@ -11,19 +10,19 @@ app.use(express.json());
 app.use(cors("*"));
 // Define a route
 app.get('/api/data', async (req, res) => {
-    const posital = req.body;
+    const zipCode = req.query.zipCode;
     try {
         const response = await positalCode.findOne({
-            postal_code: posital.zipCode
+            postal_code: zipCode
         });
         if (response) {
-            res.send({ message: "Authentication Successful", statusCode: 200, data: response })
+            res.send({ message: "Authentication Successful", statusCode: 200, data: response });
         } else {
-            res.send({ message: "Authentication Failed", statusCode: 401 })
+            res.send({ message: "Authentication Failed", statusCode: 401 });
         }
     } catch (error) {
-        console.error("Error occured when logging in", error);
-        throw error;
+        console.error("Error occurred when retrieving data", error);
+        res.status(500).send({ message: "Internal Server Error" });
     }
 });
 app.get('/', async (req, res) => {
